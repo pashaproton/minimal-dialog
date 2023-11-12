e1eb04a7bf7a697ce41e9cf8f412e0c52332fe02
Pavlo Ivanichenko <pasha.proton@gmail.com>
2023-11-11 20:44:52 -0800

    initial version

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Tooltip
+# Minimal dialog
 
-## Minimal JavaScript modal with no dependencies
+## Minimal JavaScript dialog with no dependencies
+
+To use the library just add styles and JavaScript
+```
+<link rel="stylesheet" href="...minimal-dialog.min.css">
+<script src="...minimal-dialog.min.js"></script>
+```
+
+Then bind trigger by adding `data-dialog` attribute with value of an id of a dialog.
+```
+<button data-dialog="simple-dialog">Open dialog</button>
+```
+
+And add dialog itself to a document body
+```
+<dialog id="simple-dialog">
+  <p>Simple modal content</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+    magna aliqua.</p>
+  <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <button data-dialog-close>Close</button>
+</dialog>
+```
+
+The `<button data-dialog-close>Close</button>` is optional, but you can bind any close trigger by adding `data-dialog-close` attribute to an element.
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const timeoutIds = {};
+
+const isOutsideDialog = (dialogElement, e) => {
+  const rect = dialogElement.getBoundingClientRect();
+  return (
+    e.clientX < rect.left ||
+    e.clientX > rect.right ||
+    e.clientY < rect.top ||
+    e.clientY > rect.bottom
+  );
+};
+
+const handleDialogClose = dialogElement => {
+  const dialogId = dialogElement.getAttribute("id");
+  if (!timeoutIds[dialogId]) {
+    dialogElement.classList.remove("visible");
+    timeoutIds[dialogId] = window.setTimeout(() => {
+      dialogElement.close();
+      delete timeoutIds[dialogId];
+    }, 300);
+  }
+};
+
+const handleDialogClick = (dialogElement, e) => {
+  if (isOutsideDialog(dialogElement, e)) {
+    handleDialogClose(dialogElement);
+  }
+};
+
+const bindDialogEvents = dialogElement => {
+  dialogElement.addEventListener("click", e => handleDialogClick(dialogElement, e));
+  dialogElement.querySelectorAll("[data-dialog-close]").forEach(el => {
+    el.addEventListener("click", () => handleDialogClose(dialogElement));
+  });
+};
+
+const createCloseButton = dialogElement => {
+  const closeBtn = document.createElement("a");
+  closeBtn.classList.add("close");
+  closeBtn.setAttribute("aria-label", "Close dialog");
+  closeBtn.setAttribute("title", "Close dialog");
+  closeBtn.setAttribute("type", "button");
+  closeBtn.setAttribute("data-dialog-close", "");
+  dialogElement.appendChild(closeBtn);
+};
+
+const handleTriggerClick = e => {
+  const dialogElement = document.getElementById(e.target.dataset.dialog);
+  dialogElement.showModal();
+  dialogElement.classList.add("visible");
+};
+
+const bindTriggerEvents = trigger => trigger.addEventListener("click", handleTriggerClick);
+
+const getDialogTriggers = () => document.querySelectorAll("[data-dialog]");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dialogsTriggers = getDialogTriggers();
+  const dialogsElements = document.querySelectorAll("dialog");
+  dialogsTriggers.forEach(bindTriggerEvents);
+  dialogsElements.forEach(dialogElement => {
+    createCloseButton(dialogElement);
+    bindDialogEvents(dialogElement);
+  });
+});
+
+document.addEventListener("keydown", e => {
+  if (e.key === "Escape") {
+    const openedDialog = document.querySelector("dialog.visible");
+    if (openedDialog) {
+      e.preventDefault();
+      handleDialogClose(openedDialog);
+    }
+  }
+});
